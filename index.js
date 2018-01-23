21a73d96f1f0341efee8cb59a4a885406210b0c3
Hornyák Ádám <adam.hornyak@nuage.hu>
2018-01-23 16:44:38 +0100

    database added

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ var pg = require('pg');
 var app = express();
 
 //Database config
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting users...');
-  client
-    .query('SELECT * FROM users;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
     });
+  });
 });
 
 //App start
