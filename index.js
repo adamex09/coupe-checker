fbe75e974da7ca494a7d380c0bde1b2db4d351a3
Adam Hornyak <hello@adamhornyak.com>
2021-03-23 00:27:22 +0100

    pg timeout

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ var basic = auth.basic({
 //Database config
 const { Pool, Client } = require('pg')
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL;
+  statement_timeout: 10000;
 })
 app.get('/db', function (request, response) {
   pool.connect((err, client, release) => {
